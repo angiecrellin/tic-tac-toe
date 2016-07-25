ed694115aa43c94de1efbd372c0f942f6b014387
angiecrellin <angiecrellin@gmail.com>
2016-07-25 09:38:14 -0500

    css/style.css

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+//disable after click
+//it's a tie over and over again
+
+
+
 $(document).ready(function() {
 
     var turn = 0;
@@ -12,52 +17,98 @@ $(document).ready(function() {
         [2, 4, 6, ]
     ];
 
-  var startGame = function(){
-  	$('.cell').text('');
-  }
+    var startGame = function() {
+        $('.cell').text('');
+    }
 
-  	startGame();
+    startGame();
+
+    /*function cellClick(cell) {
+    if (cell.innerHTML === "X" || cell.innerHTML === "O") {
+        return;
+    }
+}*/
+
 
     //taking turns 
     $('.gameBoard').find('.cell').on('click', function() {
+
         if (turn % 2 === 0) {
             $(this).text('X');
-        } else {
-            $(this).text('0');
+
+
+        } else if (turn % 2 !== 0) {
+            $(this).text('O');
+
         }
-        turn++;
+
+        eachTurn();
+
+        turn++
+
     })
-    
+
+    /*$('.cell').one('click', function(){
+        alert('That space is already taken')
+    })*/
+
+    /*$('.cell').bind('click', function() {
+       $(this).unbind('click');
+       alert('That space is already taken!');
+    });*/
+
+    /*$('.gameBoard').find('cell').on('click', function() {
+        if ('.cell' === 'X' || '.cell' === 'O') {
+            alert('That space is already taken');
+        }
+    })*/
+
+    $('.gameBoard').find('this').on('click', function() {
+        if ('this' === 'X' || 'this' === 'O') {
+            alert('That space is already taken');
+        }
+    })
+
 
     function getCellValue(cellNumber) {
         return $('.cell').eq(cellNumber).text();
 
     }
+
     getCellValue();
+
 
     //checking for winner
 
-    for (var i = 0; i < answers.length; i++) {
-        var answerCells = answers[i];
-        var cellAnswer = '';
-        for (var c = 0; c < answerCells.length; c++) {
-            cellAnswer += getCellValue(answerCells[c]);
-        }
-        if (cellAnswer === 'XXX') {
-            alert('X Wins!')
-            break;
-        } else if (cellAnswer === 'OOO') {
-            alert('O Wins!')
-            break;
-        } else {
-            alert('It\'s a Tie!');
-        }
+    function eachTurn() {
+        for (var i = 0; i < answers.length; i++) {
+            var answerCells = answers[i];
+            var cellAnswer = '';
+            for (var c = 0; c < answerCells.length; c++) {
+                cellAnswer += getCellValue(answerCells[c]);
+            }
+            if (cellAnswer === 'XXX') {
+                alert('X Wins!')
+                break;
+            } else if (cellAnswer === 'OOO') {
+                alert('O Wins!')
+                break;
+            } else if ($('.cell').text().length >= 9) {
+                alert('It\'s a Tie!');
+            }
 
-    }
+        }
+    };
+
 
     //new game
 
+    var newGame = function() {
+        $('.cell').text('');
+    }
 
+
+    $('.button').click(newGame);
 
 
 
